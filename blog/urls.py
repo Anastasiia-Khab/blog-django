64244e706f865390c9d0a73c8f0f228797f0aad5
Anastasiia-Khab <anastasykhab@gmail.com>
2017-12-04 14:02:31 +0200

    javascript

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^interactive', views.interactive, name='interactive'),
 ]
 
 #The last part, name='post_list', is the name of the URL that will be used to identify the view.
